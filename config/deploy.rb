4c44ebc8001a8b225be08efd5b9fd1901e798558
Takashi CHIBA <takashi@wondershake.com>
2012-10-17 17:31:59 +0900

    fixed for first release

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,8 @@ namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
+      if releases.length <= 1 ||
+          capture("cd #{latest_release} && #{source.local.log(source.next_revision(current_revision))} vendor/assets/ app/assets/ | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
